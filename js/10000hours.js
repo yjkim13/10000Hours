4de473f354ee42a4bb328a8a7d5ebb823d847560
yjkim13 <36403024+yjkim13@users.noreply.github.com>
2022-01-09 02:39:16 +0900

    Add files via upload

--- js/10000hours.js
+++ js/10000hours.js
@@ -9,18 +9,18 @@ const loading = document.querySelector(".result_loading");
 
 function calculator() {
     const fieldValue = document.querySelector("#field_value");
-    let timeValue = document.querySelector("#time_value");
-    let timeValue_int = Number(timeValue.value);
+    const timeValue = document.querySelector("#time_value");
+    const timeValue_int = Number(timeValue.value);
 
     const fieldResult = document.querySelector(".field_result");
     const timeResult = document.querySelector(".time_result");
 
     if (fieldValue.value === "") {
-        alert("입력되지 않았습니다.")
+        alert("분야가 입력되지 않았습니다.")
         fieldValue.focus();
         return false;
     } else if (timeValue.value === "") {
-        alert("입력되지 않았습니다.");
+        alert("시간이 입력되지 않았습니다.");
         timeValue.focus();
         return false
     } else if (timeValue.value > 24) {
@@ -32,10 +32,10 @@ function calculator() {
     loading.style.display = "flex";
 
     setTimeout(function () {
-        loading.style.display = "none";
-        result.style.display = "flex";
         fieldResult.innerText = fieldValue.value;
         timeResult.innerText = parseInt((10000 / timeValue_int), 10);
+        loading.style.display = "none";
+        result.style.display = "flex";
     }, 1800);
 
 }
